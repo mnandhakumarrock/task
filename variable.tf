94f6fc3b14888c7219bc615acdd7bb7a74de6596
“mnandhakumarrock” <mnandhakumarrock@gmail.com>
2024-02-16 10:52:35 +0000

    3nd commit

--- variable.tf
+++ variable.tf
@@ -12,3 +12,13 @@ variable "aws_region" {
   type        = string
   description = "AWS Region"
 }
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
